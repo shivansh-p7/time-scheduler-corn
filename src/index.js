2c7c7438e8249b7b3ab9e5462de552b2ee42e673
shivansh-p7 <shivanshsharma55364@gmail.com>
2023-02-12 23:15:46 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 const express=require('express');
 const app=express()
-const cron=require('node-cron')
+
 const route=require('./routes/route')
 app.use(express.json())
 
 
-cron.schedule("*/10 37 19 12 2 *",()=>{
-    console.log("task is running in every 10 seconds")
-})
+
 
 
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,34 @@
 const router=require('express').Router()
+const cron=require('node-cron')
 
 
 
 
 router.post("/scheduler",(req,res)=>{
+try{
+    let data=req.body
+    let {text, dateTime}=data
+ 
+    let seconds=Number(text.length+"000")
+dateTime= dateTime.split(" ")
+let date=dateTime[0].split("-").map(x=>Number(x))
+let time=dateTime[1].split(":").map(x=>Number(x))
+console.log(date,time)
 
-res.send("hi")
+   cron.schedule(`${time[1]} ${time[0]} ${date[2]} ${date[1]} *`,()=>{
+    setTimeout(()=>{
+      return res.send(text)
+      
+          },seconds)
+    })
+  
+
+}catch(error){
+
+return res.status(500).send({status:false,message:error.message})
+
+}
+
 
 
 })
